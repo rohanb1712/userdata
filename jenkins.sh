6fcc28705586568fb4899f1e62ab864067dd180f
rohanb1712 <148258221+rohanb1712@users.noreply.github.com>
2024-01-21 12:06:50 +0530

    Update and rename jenkins to jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Update and install Java
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+java -version
+
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
